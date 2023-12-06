a57bef3164d69aeafbaf6ea21cb2005407df0cd7
Даниил <113928056+gigDani@users.noreply.github.com>
2023-12-06 10:37:21 +0300

    1 comit

--- .idea/Main.java
+++ .idea/Main.java
@@ -1,5 +1,5 @@
 class Main{
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке main");
+        System.out.print("Это добавлено в ветке main -1 коммит");
     }
 }
